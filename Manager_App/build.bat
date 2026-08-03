658b1efa4b3a8e44df643c67236a1b7e6c8a451b
EliteSoftware - Zach <123858247+TheShadyRainbow4@users.noreply.github.com>
2026-08-03 17:44:30 -0400

    Inject explicit PE Header VersionInfo to prevent MinGW from injecting garbage descriptors in Windows Explorer

--- Manager_App/build.bat
+++ Manager_App/build.bat
@@ -1,6 +1,7 @@
 @echo off
 echo Building Manager App...
-g++ main.cpp -o Elite_App_Marketplace-Server.exe -mwindows -lcomctl32 -lws2_32 -static
+windres resource.rc -O coff -o resource.res
+g++ main.cpp resource.res -o Elite_App_Marketplace-Server.exe -mwindows -lcomctl32 -lws2_32 -static
 if %errorlevel% equ 0 (
     echo Build successful: Elite_App_Marketplace-Server.exe
 ) else (
